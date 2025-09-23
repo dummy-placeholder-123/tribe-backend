0010d094f7f909e606267ceb7040f81cb6435f3f
Devashish Rane <ranedevashish2@gmail.com>
2025-09-23 14:21:45 +0530

    Ensure refreshed JWTs carry unique identifiers

--- spring-webapi/src/main/java/com/tribe/backend/TribeApplication.java
+++ spring-webapi/src/main/java/com/tribe/backend/TribeApplication.java
@@ -3,11 +3,9 @@ package com.tribe.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableAsync
 public class TribeApplication {
 
     public static void main(String[] args) {
